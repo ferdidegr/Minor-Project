719474042a68bc5dbb03481fa4b4caa1cedd8717
Mirandavd <mbrvandoorn@student.tudelft.nl>
2013-11-14 12:23:12 +0100

    Testprogrammatjes

--- src/tests/SimpleOGLRenderer.java
+++ src/tests/SimpleOGLRenderer.java
@@ -17,11 +17,27 @@ public class SimpleOGLRenderer {
 		
 		// Initialization Code OpenGL
 		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, 640, 480, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 		
-		
-		// Renderloop
+		// Render loop
 		while (!Display.isCloseRequested()) {
 			// Render
+			
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Last one only for 3D
+			
+			glBegin(GL_QUADS);
+				glVertex2i(400, 400); // Upper Left
+				glVertex2i(450, 400); // Upper right
+				glVertex2i(450, 450); // Bottom right
+				glVertex2i(400, 450); // Bottom left
+			glEnd();
+			
+			glBegin(GL_LINES);
+				glVertex2i(100, 100);
+				glVertex2i(200, 200);
+			glEnd();
 			
 			Display.update();
 			Display.sync(60);
@@ -30,8 +46,7 @@ public class SimpleOGLRenderer {
 		Display.destroy();
 	}
 	public static void main(String[] args) {
-		new HelloWorld();
-
+		new SimpleOGLRenderer();
 	}
 
 }
