20655c5a32b2a9979ee4e356ea791aa9053a1c49
Zhi-Li <zhi-li_liu@live.nl>
2013-11-14 18:03:09 +0100

    Succesfully obtained absolute path

--- src/Editor/JfilechooserTest.java
+++ src/Editor/JfilechooserTest.java
@@ -1,5 +1,21 @@
 package Editor;
 
-public class JfilechooserTest {
+import java.io.File;
 
+import javax.swing.JFileChooser;
+
+public class JfilechooserTest {
+	public void start(){
+		JFileChooser jfc = new JFileChooser(System.getProperty("user.dir"));
+		int res =jfc.showSaveDialog(null);
+		if(res==JFileChooser.APPROVE_OPTION){
+			File file = jfc.getSelectedFile();
+			System.out.println(file.getAbsolutePath());
+		}
+	}
+	
+	public static void main(String[] args){
+		JfilechooserTest jft = new JfilechooserTest();
+		jft.start();
+	}
 }
