edb5834ec52548af74f72c35685d8a775f7829a4
Mirandavd <mbrvandoorn@student.tudelft.nl>
2013-11-14 09:14:30 +0100

    Menu class added

---
+++ src/Menu.java
@@ -0,0 +1,30 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Menu {
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.create();
+		}
+		catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		// init OpenGL here
+		
+		while (!Display.isCloseRequested()) {
+			
+			// render OpenGL here
+			
+			Display.update();
+		}
+		Display.destroy();
+	}
+	public static void main(String[] argv) {
+		Menu displayExample = new Menu();
+		displayExample.start();
+	}
+}
