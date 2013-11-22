328add035d6533dced4ac563c5ac486f9eb9208d
Zhi-Li <zhi-li_liu@live.nl>
2013-11-22 17:51:49 +0100

    Added comments

--- src/Game/Vector.java
+++ src/Game/Vector.java
@@ -1,5 +1,10 @@
 package Game;
-
+/**
+ * Vector class
+ * 
+ * @author ZL
+ *
+ */
 public class Vector {
 	private double x, y, z;
 	
@@ -10,36 +15,55 @@ public class Vector {
 	}
 	/**
 	 * Vector addition
-	 * @param vec
+	 * @param vec another Vector object
 	 */
 	public void add(Vector vec){
 		this.x+=vec.x;
 		this.y+=vec.y;
 		this.z+=vec.z;
 	}
-	
+	/**
+	 * Vector addition with splitted cartesian components
+	 * @param x	
+	 * @param y
+	 * @param z
+	 */
 	public void add(double x, double y ,double z){
 		this.x+=x;
 		this.y+=y;
 		this.z+=z;
 	}
+	/**
+	 * Scale each component independently
+	 * @param factor1
+	 * @param factor2
+	 * @param factor3
+	 */
 	public void scale(double factor1, double factor2, double factor3){
 		this.x*=factor1;
 		this.y*=factor2;
 		this.z*=factor3;
 	}
+	/**
+	 * Scale all components with the same factor
+	 * @param factor
+	 */
 	public void scale(double factor){
 		this.x*=factor;
 		this.y*=factor;
 		this.z*=factor;
 	}
-	
+	/**
+	 * reset the vector to a zero vector
+	 */
 	public void setZero(){
 		this.x=0;
 		this.y=0;
 		this.z=0;
 	}
-	
+	/**
+	 * Describes the vector with its components
+	 */
 	public String toString(){
 		return x+", "+y+", "+z;
 	}
