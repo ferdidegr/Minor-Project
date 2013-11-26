76d6cfbda068fc2b3f470347b5d1fbcfbbc199f5
karinvangarderen <K.A.vangarderen@student.tudelft.nl>
2013-11-26 11:21:16 +0100

    Even klaar met tekst

--- src/Menu/MenuButton.java
+++ src/Menu/MenuButton.java
@@ -30,6 +30,7 @@ public class MenuButton {
 	public static int width, height;
 	public static int mousex, mousey;
 	private TrueTypeFont myfont;
+	private Text content;
 	private String name;
 	/**
 	 * Constructor
@@ -45,8 +46,9 @@ public class MenuButton {
 		this.mouseover = Mouseover;
 		this.ID = ID;
 		this.name = name;
+		this.content = new Text(0.7 * height, name);
 		//TODO: Volgens mij kan dit beter ergens anders gebeuren, bedenken waar!
-		initFont();
+		content.initFont();
 	}
 	/**
 	 * draw the buttons
@@ -76,49 +78,10 @@ public class MenuButton {
 		glDisable(GL_TEXTURE_2D);
 		glDisable(GL_BLEND);
 		
-		text(x +width/2, y+height/2, 1, name );
+		content.draw(x +width/2, y+height/2, 1);
+
 	}
 	
-	/** Schrijft tekst vanaf de coordinaten x,y met tekst s
-	 *  
-	 * @param x
-	 * @param y
-	 * @param scale
-	 * @param s
-	 */
-	public void text(int x, int y,float scale, String s){
-		int lineh = myfont.getHeight();
-		int width = myfont.getWidth(s);
-
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glPushMatrix();
-		GL11.glTranslatef(x - width/2, y + lineh/2, 0);
-		GL11.glScalef(scale, -scale, scale);
-		myfont.drawString(0, 0, s,Color.white);
-		GL11.glPopMatrix();
-		GL11.glDisable(GL11.GL_BLEND);
-	}
-	
-	/** Initialiseert het font
-	 *  !Definieer hier het juiste font bestand!
-	 * 
-	 */
-	public void initFont(){
-			try {
-			Double size = new Double(0.7 * height);
-			float fontsize = size.floatValue();
-			InputStream inputStream	= ResourceLoader.getResourceAsStream("BEBAS.ttf");
-
-			Font awtFont2 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
-			awtFont2 = awtFont2.deriveFont(fontsize); // set font size
-			myfont = new TrueTypeFont(awtFont2, true);
-		
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 	/**
 	 * checks if it is in this button
 	 * @param x
