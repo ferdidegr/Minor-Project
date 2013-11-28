1d0119797055f27d6dc4ca11aaa1856f888c662f
Mirandavd <mbrvandoorn@student.tudelft.nl>
2013-11-28 09:45:02 +0100

    objloader apart

--- src/ModelLoader/Face.java
+++ src/ModelLoader/Face.java
@@ -5,12 +5,16 @@ import org.lwjgl.util.vector.Vector3f;
 public class Face {
 	public Vector3f vertex = new Vector3f(); // three indices, not vertices or normals
 	public Vector3f normal = new Vector3f();
-	public Vector3f texture = new Vector3f();
 
-	public Face(Vector3f vertex, Vector3f texture, Vector3f normal) {
+	public Face(Vector3f vertex, Vector3f normal) {
 		this.vertex = vertex;
-		this.texture = texture;
 		this.normal = normal;
 	}
+
+	public Vector3f getVertex() {return vertex;}
+	public void setVertex(Vector3f vertex) {this.vertex = vertex;}
+
+	public Vector3f getNormal() {return normal;}
+	public void setNormal(Vector3f normal) {this.normal = normal;}
 }
 
--- src/ModelLoader/Model.java
+++ src/ModelLoader/Model.java
@@ -3,39 +3,22 @@ package ModelLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Model {
 	public List<Vector3f> vertices = new ArrayList<Vector3f>();
 	public List<Vector3f> normals = new ArrayList<Vector3f>();
-	public List<Vector2f> textures = new ArrayList<Vector2f>();
 	public List<Face> faces = new ArrayList<Face>();
 
 	public Model() {}
 
-	public List<Vector3f> getVertices() {
-		return vertices;
-	}
+	public List<Vector3f> getVertices() {return vertices;}
+	public void setVertices(List<Vector3f> vertices) {this.vertices = vertices;}
 
-	public void setVertices(List<Vector3f> vertices) {
-		this.vertices = vertices;
-	}
+	public List<Vector3f> getNormals() {return normals;}
+	public void setNormals(List<Vector3f> normals) {this.normals = normals;}
 
-	public List<Vector3f> getNormals() {
-		return normals;
-	}
-
-	public void setNormals(List<Vector3f> normals) {
-		this.normals = normals;
-	}
-
-	public List<Face> getFaces() {
-		return faces;
-	}
-
-	public void setFaces(List<Face> faces) {
-		this.faces = faces;
-	}
+	public List<Face> getFaces() {return faces;}
+	public void setFaces(List<Face> faces) {this.faces = faces;}
 }
 
