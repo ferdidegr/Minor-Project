ed9dfef1ce8e879ad725a43b3634fe689f8d44fb
Mirandavd <mbrvandoorn@student.tudelft.nl>
2013-11-28 12:05:46 +0100

    Moved textures editor

--- src/Editor/Textures.java
+++ src/Editor/Textures.java
@@ -11,21 +11,21 @@ public class Textures {
 	
 	static {
 		try {
-			texempty = IO.readtexture("res/empty.jpg");
-			texmenubar = IO.readtexture("res/Wooden_floor_original.jpg");
-			texnewmaze = IO.readtexture("res/newmaze.jpg");
-			texwall1 = IO.readtexture("res/wall1.jpg");
-			texwall2 = IO.readtexture("res/wall2.jpg");
-			texwall3 = IO.readtexture("res/wall3.jpg");
-			texwall4 = IO.readtexture("res/wall4.jpg");
-			texwall5 = IO.readtexture("res/wall5.jpg");
-			texwall6 = IO.readtexture("res/wall6.jpg");
-			texwall7 = IO.readtexture("res/wall7.jpg");
-			texsave = IO.readtexture("res/save.jpg");
-			texload = IO.readtexture("res/load.jpg");
-			texspike = IO.readtexture("res/spikes.jpg");
-			texflaggreen= IO.readtexture("res/flaggreen.jpg");
-			texflagred= IO.readtexture("res/flagred.jpg");
+			texempty = IO.readtexture("res/Textures/Editor/empty.jpg");
+			texmenubar = IO.readtexture("res/Textures/Editor/Wooden_floor_original.jpg");
+			texnewmaze = IO.readtexture("res/Textures/Editor/newmaze.jpg");
+			texwall1 = IO.readtexture("res/Textures/Editor/wall1.jpg");
+			texwall2 = IO.readtexture("res/Textures/Editor/wall2.jpg");
+			texwall3 = IO.readtexture("res/Textures/Editor/wall3.jpg");
+			texwall4 = IO.readtexture("res/Textures/Editor/wall4.jpg");
+			texwall5 = IO.readtexture("res/Textures/Editor/wall5.jpg");
+			texwall6 = IO.readtexture("res/Textures/Editor/wall6.jpg");
+			texwall7 = IO.readtexture("res/Textures/Editor/wall7.jpg");
+			texsave = IO.readtexture("res/Textures/Editor/save.jpg");
+			texload = IO.readtexture("res/Textures/Editor/load.jpg");
+			texspike = IO.readtexture("res/Textures/Editor/spikes.jpg");
+			texflaggreen= IO.readtexture("res/Textures/Editor/flaggreen.jpg");
+			texflagred= IO.readtexture("res/Textures/Editor/flagred.jpg");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
