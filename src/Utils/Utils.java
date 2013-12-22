7a616b1b587fe6053c138b6847e029e5d0ffc40e
Zhi-Li <zhi-li_liu@live.nl>
2013-12-22 15:50:15 +0100

    added screenshot option

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -2,11 +2,18 @@ package Utils;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.Display;
+
 
 public class Utils {
 	public static void glNormalvec(double[] vec){
@@ -33,5 +40,35 @@ public class Utils {
 		}
 		
 		return MazeList;
+	}
+	
+	public static void makeScreenShot(){
+		glReadBuffer(GL_FRONT);
+		int width = Display.getDisplayMode().getWidth();
+		int height= Display.getDisplayMode().getHeight();
+		int bpp = 4; // Assuming a 32-bit display with a byte each for red, green, blue, and alpha.
+		ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * bpp);
+		glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer );
+		
+		File file = new File("screenshots//screenshot-"+getTime.getCurrentTimeStamp("YYYMMDDHHmmss")+".png"); // The file to save to.
+		if(!file.exists()){file.mkdirs();}
+		String format = "PNG"; // Example: "PNG" or "JPG"
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		  
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++)
+			{
+				int i = (x + (width * y)) * bpp;
+				int r = buffer.get(i) & 0xFF;
+				int g = buffer.get(i + 1) & 0xFF;
+				int b = buffer.get(i + 2) & 0xFF;
+				image.setRGB(x, height - (y + 1), (0xFF << 24) | (r << 16) | (g << 8) | b);
+			}
+		}
+		  
+		try {
+			ImageIO.write(image, format, file);			
+		} catch (IOException e) { }
+			
 	}
 }
