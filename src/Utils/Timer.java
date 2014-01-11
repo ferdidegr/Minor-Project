f827ff22ff03b4ab484abaae2a64d2dd5e9f1a44
Zhi-Li <zhi-li_liu@live.nl>
2014-01-11 12:02:40 +0100

    when not started, getTime will return 0;

--- src/Utils/Timer.java
+++ src/Utils/Timer.java
@@ -3,7 +3,7 @@ import org.lwjgl.Sys;
 
 public class Timer {
 	private long ms = 0;
-	private long initTime ;
+	private long initTime = 0;
 	private long currentTime;
 	
 	public Timer(){
@@ -17,7 +17,9 @@ public class Timer {
 	}
 	
 	public long getTime(){
+		if(initTime !=0)
 		return (Sys.getTime()*1000)/Sys.getTimerResolution()-initTime;
+		return 0;
 	}	
 	
 }
